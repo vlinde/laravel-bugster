fb1a5885da9b79a3922b573553a3dc6d82d7add2
Dobrean Marian <dobrean.marian@vlinde.com>
2025-03-18 09:57:13 +0200

    Update config file

--- config/bugster.php
+++ config/bugster.php
@@ -1,19 +1,14 @@
 <?php
 
 return [
-    // available; redis, db, file
     'log_driver' => 'redis',
 
-    // Set log channel
     'log_channel' => 'daily',
 
-    // Set the redis connection to save the log in Redis
     'redis_connection_name' => 'default',
 
-    // Redis connection to check if queues are working
     'redis_connection_for_queues_status' => 'default',
 
-    // Microsoft Teams webhook for notification
     'microsoft_team_hook' => env('MS_TEAMS_WEBHOOK_URL'),
 
     'statistic_keys' => [],
