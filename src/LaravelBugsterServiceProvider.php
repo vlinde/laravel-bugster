08f5ba46c98f425853e0d3ceca033f4aaaedcaa9
Dobrean Marian <dobrean.marian@vlinde.com>
2026-02-27 11:37:15 +0200

    Fix migration publish

--- src/LaravelBugsterServiceProvider.php
+++ src/LaravelBugsterServiceProvider.php
@@ -37,8 +37,8 @@ class LaravelBugsterServiceProvider extends ServiceProvider
             __DIR__.'/Database/Migrations/remove_laravel_bugster_stats_table.php' => database_path('migrations/'.date('Y_m_d_His', time() + 3).'_remove_laravel_bugster_stats_table.php'),
             __DIR__.'/Database/Migrations/create_laravel_bugster_status_codes_table.php' => database_path('migrations/'.date('Y_m_d_His', time() + 4).'_create_laravel_bugster_status_codes_table.php'),
             __DIR__.'/Database/Migrations/update_fields_to_laravel_bugster_notifications_table.php' => database_path('migrations/'.date('Y_m_d_His', time() + 5).'_update_fields_to_laravel_bugster_notifications_table.php'),
-            __DIR__.'/Database/Migrations/create_laravel_bugster_webhooks_table.php' => database_path('migrations/'.date('Y_m_d_His', time() + 6).'create_laravel_bugster_webhooks_table.php'),
-            __DIR__.'/Database/Migrations/add_type_to_laravel_bugster_webhooks_table.php' => database_path('migrations/'.date('Y_m_d_His', time() + 7).'add_type_to_laravel_bugster_webhooks_table.php'),
+            __DIR__.'/Database/Migrations/create_laravel_bugster_webhooks_table.php' => database_path('migrations/'.date('Y_m_d_His', time() + 6).'_create_laravel_bugster_webhooks_table.php'),
+            __DIR__.'/Database/Migrations/add_type_to_laravel_bugster_webhooks_table.php' => database_path('migrations/'.date('Y_m_d_His', time() + 7).'_add_type_to_laravel_bugster_webhooks_table.php'),
         ], 'bugster-migrations');
 
         // Publishing is only necessary when using the CLI.
